c8fbda456f52390f80452a2d4646cd6571c5a494
MathGuy571 <52160373+MathGuy571@users.noreply.github.com>
2019-11-14 22:37:10 +0200

    Create Joystick.js

---
+++ Joystick.js
@@ -0,0 +1,52 @@
+class Joystick {
+    constructor() {
+        //joypad
+        this.p = new Vector(W/4, 5*H/6);
+        this.pr = 40;
+        //joystick
+        this.s = new Vector(W/4, 5*H/6);
+        this.sr = 20;
+        
+        this.down = false;
+        this.nv = new Vector(0, 0);
+    }
+    
+    start(e) {
+        this.down = true;
+        this.move(e);
+    }
+    
+    move(e) {
+        this.s.x = e.touches[0].clientX;
+        this.s.y = e.touches[0].clientY;
+        
+        //contain joystick within joypad
+        let dist = min(hypot(this.s.x - this.p.x, this.s.y - this.p.y), this.pr);
+        let θ = atan2(this.s.y - this.p.y, this.s.x - this.p.x);
+        this.s.x = this.p.x + dist * cos(θ);
+        this.s.y = this.p.y + dist * sin(θ);
+        
+        //convert user input to player movement
+        this.nv.x = this.s.x - this.p.x;
+        this.nv.y = this.s.y - this.p.y;
+        //normalise the vector
+        this.nv.norm();
+        //scale the normalised vector according to joystick distance from joypad center
+        let maxMove = 3; //max movement speed
+        let mapedDist = map(dist, 0, this.pr, 0, maxMove);
+        this.nv.scale(mapedDist);
+    }
+    
+    end(e) {
+        this.down = false;
+        this.s.x = W/4;
+        this.s.y = 5*H/6;
+    }
+    
+    draw() {
+        //joypad
+        circle(this.p.x, this.p.y, this.pr, "rgba(0, 100, 255, 0.6)", false);
+        //joystick
+        circle(this.s.x, this.s.y, this.sr, "rgba(255, 100, 0, 0.6)", false);
+    }
+}
