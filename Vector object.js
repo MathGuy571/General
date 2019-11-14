05524755c8f5bc1a6f95eff51bca38c40e2062f2
MathGuy571 <52160373+MathGuy571@users.noreply.github.com>
2019-11-14 22:11:05 +0200

    Create Vector object.js

---
+++ Vector object.js
@@ -0,0 +1,33 @@
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    add(v) {
+        this.x += v.x;
+        this.y += v.y;
+        return this;
+    }
+    
+    subtract(v) {
+        this.x -= v.x;
+        this.y -= v.y;
+        return this;
+    }
+    
+    scale(n) {
+        this.x *= n;
+        this.y *= n;
+    }
+    
+    magn() {
+        return hypot(this.x, this.y);
+    }
+    
+    norm() {
+        let mg = this.magn();
+        this.x /= mg;
+        this.y /= mg;
+    }
+}
