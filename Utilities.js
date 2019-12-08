27ff0b961f2b8d907d628a21e6282759badfe772
MathGuy571 <52160373+MathGuy571@users.noreply.github.com>
2019-12-08 14:33:32 +0200

    Minor edit at equivalent angle function

--- Utilities.js
+++ Utilities.js
@@ -50,7 +50,7 @@ const equivAngle = (θ) => {
     }
     else if(θ > (2*Math.PI)) {
         return θ % (2*Math.PI);
-    } else if(θ < 0 && θ> -2*Math.PI) {
+    } else if(θ < 0 && θ > -2*Math.PI) {
         return 2*Math.PI - (-θ);
     } else if(θ < (-2*Math.PI)) {
         return 2*Math.PI - ((-θ) % (2*Math.PI));
